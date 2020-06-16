afc1e1b2675ec81bdc720c9ec178f24c112824e6
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-16 16:45:07 -0300

    Consumindo Query params

--- Modulo01/index.js
+++ Modulo01/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const server = express();
 
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Thiago"}
+
 server.get('/teste', (req, res)=> {
-  res.send('Hello World');
+
+  const nome = req.query.nome;
+
+  return res.json({message: `Hello ${nome}`})
 });
 
 server.listen(3000);
