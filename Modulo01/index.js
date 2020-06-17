f0805e5ee335870e92336cd2be4e1122902788c5
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-17 17:17:09 -0300

    Criando usuário

--- Modulo01/index.js
+++ Modulo01/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Thiago"}
 
 const users = ['João', 'Ana', 'Pedro', 'Maria'];
+
+server.get('/users', (req, res) =>{
+  return res.json(users);
+})
 
 server.get('/users/:index', (req, res)=> {
   const {index} = req.params;
@@ -14,4 +20,11 @@ server.get('/users/:index', (req, res)=> {
   return res.json(users[index]);
 });
 
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
 server.listen(3000);
